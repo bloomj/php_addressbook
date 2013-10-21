693d8f2cdb4161ef34efb0d49cd01204b0b64717
James Bloom <jcbloom@gmail.com>
2013-10-21 08:29:09 -0700

    Issue #6: Added default DB to properties

--- _includes/db.inc.php
+++ _includes/db.inc.php
@@ -14,12 +14,14 @@ $ini_array = parse_ini_file($prop_file_path, true);
 $db_ip = $ini_array["db_config"]["db_ip"];
 $db_user = $ini_array["db_config"]["db_user"];
 $db_pass = $ini_array["db_config"]["db_pass"];
+$db_default = $ini_array["db_config"]["db_default"];
 
 echo 'Got IP: "'.$db_ip.'".<br/>';
 //echo 'Got user: "'.$db_user.'".<br/>';
 //echo 'Got pass: "'.$db_pass.'".<br/>';
+echo 'Got Database: "'.$db_default.'".<br/>';
 
-$db_handle = mysql_connect($db_ip,$db_user,$db_pass);
+$db_handle = mysql_connect($db_ip,$db_user,$db_pass,$db_default);
 if($db_handle===false) {
 	echo 'ERROR: Could not connect to mysql database at "'.$db_ip.'".<br/>';
     if($CONF['debug']) { 
