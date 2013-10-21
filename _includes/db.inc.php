a057d2628206977b8d5ebc584f6ef41cf7fd1fb0
James Bloom <jcbloom@gmail.com>
2013-10-21 07:46:38 -0700

    Issue #6: loading db properties for MySQL connectivity

--- _includes/db.inc.php
+++ _includes/db.inc.php
@@ -1,10 +1,14 @@
 <?php 
 
-extract(parse_ini_file("../resources/properties.ini"));
+$ini_array = parse_ini_file("../resources/properties.ini");
 
-echo 'Got IP: '.$db_ip;
-echo 'Got user: '.$db_user;
-echo 'Got pass: '.$db_pass;
+$db_ip = $ini_array["db_config"]["db_ip"];
+$db_user = $ini_array["db_config"]["db_user"];
+$db_pass = $ini_array["db_config"]["db_pass"];
+
+echo 'Got IP: "'.$db_ip.'".<br/>';
+echo 'Got user: "'.$db_user.'".<br/>';
+echo 'Got pass: "'.$db_pass.'".<br/>';
 
 $db_handle = mysql_connect($db_ip,$db_user,$db_pass);
 if($db_handle===false) {
