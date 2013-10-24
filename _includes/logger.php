72078c36bc0e9e63c9d7f4d49d192c23f164fb7b
James Bloom <jcbloom@gmail.com>
2013-10-24 11:12:43 -0700

    Issue #11: fixed path issues with log4php config

--- _includes/logger.php
+++ _includes/logger.php
@@ -3,7 +3,11 @@
 include_once('log4php/Logger.php');
 
 // Tell log4php to use our configuration file.
-$log4php_config_path = $_SERVER['DOCUMENT_ROOT']."/resources/log4php_config.xml";
+$root_path = "/var/www";
+if($_SERVER['DOCUMENT_ROOT'] != "") {
+	$root_path = $_SERVER['DOCUMENT_ROOT'];
+}
+$log4php_config_path = $root_path."/resources/log4php_config.xml";
 Logger::configure($log4php_config_path);
 
 // Fetch a logger, it will inherit settings from the root logger
