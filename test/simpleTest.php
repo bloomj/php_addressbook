bb25916638a78a128fdb5fbed2ea04cf7c0474a5
James Bloom <jcbloom@gmail.com>
2013-10-24 10:36:46 -0700

    Issue #11: added setup/teardown and an additional test case

--- test/simpleTest.php
+++ test/simpleTest.php
@@ -1,6 +1,22 @@
 <?php
 class simpleTest extends PHPUnit_Framework_TestCase
 {
+	public static function setUpBeforeClass() {
+		echo 'Setting up simpleTest class'.PHP_EOL;
+	}
+	
+	public static function tearDownAfterClass() {
+		echo 'Tearing down simpleTest class'.PHP_EOL;
+	}
+	
+	protected function setUp() {
+		echo 'Setting up function'.PHP_EOL;
+	}
+	
+	protected function tearDown() {
+		echo 'Tearing down function'.PHP_EOL;
+	}
+	
     public function testStringComparison()
     {
         $testStr = "test123";
@@ -15,6 +31,11 @@ class simpleTest extends PHPUnit_Framework_TestCase
         
         echo 'Checking string with assertTrue'.PHP_EOL;
         $this->assertTrue($testStr == "test123");
+    }
+    
+    public function testMath() {
+    	echo 'Check 2+2=4'.PHP_EOL;
+    	$this->assertEquals(4, 2+2);
     }
 }
 ?>
