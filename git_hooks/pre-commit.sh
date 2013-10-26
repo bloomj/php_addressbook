b52034f0dfd3d133ff2375ef5f3b132e9c5c1c55
James Bloom <jcbloom@gmail.com>
2013-10-26 02:56:19 -0700

    Issue #11: add git pre-commit hook

---
+++ git_hooks/pre-commit.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/php
+<?php
+echo PHP_EOL;
+
+// output a little introduction
+echo '>> Starting unit tests' . PHP_EOL;
+
+// get the name for this project; probably the topmost folder name
+$curDir = getcwd();
+$projectName = basename(getcwd());
+//echo 'Running from path:'.$curDir;
+
+// execute unit tests (it is assumed that a phpunit.xml configuration is present 
+// in the root of the project)
+exec('phpunit -v -c '.$curDir.'/resources/phpunit_config.xml', $output, $returnCode);
+
+// if the build failed, output a summary and fail
+if ($returnCode !== 0) {
+	$testFailures = "";
+	$startCopy = false;
+
+    // find the line with the summary; this might not be the last
+    while (($curLine = array_pop($output)) !== null) {
+        if (strpos($curLine, 'Tests:') !== false) {
+            $minimalTestSummary = $curLine;
+			$startCopy = true;
+        }
+		
+		if($startCopy) {
+			$testFailures = $curLine.PHP_EOL.$testFailures;
+			
+			if(strpos($curLine, 'There was') !== false) {
+				break;
+			}
+		}
+    }
+
+    // output the status
+    echo '>> Test suite for ' . $projectName . ' failed:' . PHP_EOL;
+    echo $testFailures;
+	//echo $minimalTestSummary;
+    echo chr(27) . '[0m' . PHP_EOL; // disable colors and add a line break
+    echo PHP_EOL;
+
+    // abort the commit
+    exit(1);
+}
+
+echo '>> All tests for ' . $projectName . ' passed.' . PHP_EOL;
+echo PHP_EOL;
+exit(0);
