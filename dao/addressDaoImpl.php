08895a2b375ad421ba05e5cb76852506394f8138
James Bloom <jcbloom@gmail.com>
2014-03-04 07:00:30 -0800

    Issue #37: Implemented store, read, update functions of Address Serializable DAO

--- dao/addressDaoImpl.php
+++ dao/addressDaoImpl.php
@@ -14,7 +14,17 @@ Logger::configure($log4php_config_path);
  *
  */
 class AddressImpl implements IAddressDAO {
+	/**
+	 * Logger
+	 * @var Logger
+	 */
 	protected static $log;
+	
+	/**
+	 * Data directory for Address data files
+	 * @var String
+	 */
+	private static $dataDir = "";
 	
 	public function __construct() {
 		$this->log = Logger::getLogger('unittest');
@@ -24,28 +34,81 @@ class AddressImpl implements IAddressDAO {
 	 * Create or update an Address object.
 	 * 
 	 * @param Address $address
+	 * @throws Exception
+	 * @return boolean
 	 */
 	public function store($address) {
 		$this->log->info("Storing address: ".$address->getID());
 		
+		$success = false;
+		$file_name = 'address_'.$address->getID();
+		try {
+			$handle = fopen($file_name, 'w') or die('Cannot open file:  '.$file_name);
+			fwrite($handle, $address->serialize());
+			$success = true;
+		}
+	    catch(Exception $e) {
+            $this->log->error("Exception writing file: ".$file_name, $e);
+            throw $e;
+        }
+
+		fclose($handle);
+		
+		return $success;
 	}
 	
 	/**
 	 * Read an Address object.
 	 * 
 	 * @param String $id
+	 * @throws Exception
+	 * @return Address
+	 * 
 	 */
 	public function read($id) {
 		$this->log->info("Reading address: ".$id);
+		
+		$file_name = 'address_'.$id;
+		
+		if(!file_exists($file_name)) {
+			throw new Exception("File: ".$file_name." does not exist");
+		}
+		
+		$address = new Address();
+		try {
+			$handle = fopen($file_name, 'r');
+			$address->unserialize(fread($handle,filesize($file_name)));
+		}
+		catch(Exception $e) {
+			$this->log->error("Exception reading file: ".$file_name, $e);
+			throw $e;
+		}
+		
+		return $address;
 	}
 	
 	/**
 	 * Delete an Address object.
 	 * 
 	 * @param String $id
+	 * @throws Exception
+	 * @return boolean
 	 */
 	public function delete($id) {
 		$this->log->info("Deleting address: ".$id);
+		
+		$success = false;
+		$file_name = 'address_'.$id;
+		try {
+			unlink($file_name);
+			$success = true;
+		}
+		catch(Exception $e) {
+			$this->log->error("Exception deleting file: ".$file_name, $e);
+			throw $e;
+		}
+		
+		return $success;
 	}
 }
 ?>
